48673ccc79dac2db4f9e2fb754c097688139c072
connordeaneuz <connordeane@urbanzoo.io>
2024-11-20 22:01:28 +0000

    init commit

---
+++ test/HelloWorld.js
@@ -0,0 +1,12 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Print Hello World", function () {
+  it("Should return the string 'Hello, world!'", async function () {
+    const HelloWorld = await ethers.getContractFactory("HelloWorld");
+    const helloWorld = await HelloWorld.deploy("Hello, world!");
+
+    expect(await helloWorld.message()).to.equal("Hello, world!");
+
+  });
+});
